16bfd6d8fafaaf3a9b8bb04f3630d4faa6970028
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-13 11:51:11 +0200

    Update feed.php

--- controllers/front/feed.php
+++ controllers/front/feed.php
@@ -5,16 +5,21 @@ class GoogleMerchantFeedModuleFrontController extends ModuleFrontController
     {
         parent::initContent();
 
+        ob_start(); // Start output buffering
+
         $feed = $this->module->generateFeed();
 
         if ($feed === false) {
             header('HTTP/1.1 500 Internal Server Error');
             echo 'Error generating feed. Please check logs for more details.';
+            ob_end_flush(); // Flush the buffer and turn off output buffering
             exit;
         }
 
         header('Content-Type: application/xml');
         echo $feed;
+
+        ob_end_flush(); // Flush the buffer and turn off output buffering
         exit;
     }
 }
