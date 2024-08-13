615b29310d1dc5eceacbbdc3c102fd0d7126f551
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-13 13:43:59 +0200

    Update googlemerchant.php

--- googlemerchant.php
+++ googlemerchant.php
@@ -1,4 +1,5 @@
 <?php
+
 if (!defined('_PS_VERSION_')) {
     exit;
 }
@@ -17,7 +18,8 @@ class googlemerchant extends Module
         parent::__construct();
 
         $this->displayName = $this->l('Google Merchant Center Feed');
-        $this->description = $this->l('Generate a product feed for Google Merchant Center');
+        $this->description = $this->l('Generate a product feed for Google Merchant Center.');
+
         $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
     }
 
@@ -36,25 +38,23 @@ class googlemerchant extends Module
     {
         $output = null;
 
-        if (Tools::isSubmit('submit'.$this->name)) {
-            $feed_url = strval(Tools::getValue('GOOGLEMERCHANT_FEED_URL'));
-            if (!$feed_url || empty($feed_url)) {
-                $output .= $this->displayError($this->l('Invalid Feed URL'));
+        if (Tools::isSubmit('submit' . $this->name)) {
+            $url = strval(Tools::getValue('GOOGLEMERCHANT_FEED_URL'));
+            if (!$url || empty($url)) {
+                $output .= $this->displayError($this->l('Invalid URL value'));
             } else {
-                Configuration::updateValue('GOOGLEMERCHANT_FEED_URL', $feed_url);
+                Configuration::updateValue('GOOGLEMERCHANT_FEED_URL', $url);
                 $output .= $this->displayConfirmation($this->l('Settings updated'));
             }
         }
 
-        return $output.$this->displayForm();
+        return $output . $this->displayForm();
     }
 
     public function displayForm()
     {
-        // Get default language
         $default_lang = (int)Configuration::get('PS_LANG_DEFAULT');
 
-        // Init Fields form array
         $fields_form[0]['form'] = array(
             'legend' => array(
                 'title' => $this->l('Settings'),
@@ -75,53 +75,19 @@ class googlemerchant extends Module
         );
 
         $helper = new HelperForm();
+
         $helper->module = $this;
         $helper->name_controller = $this->name;
         $helper->identifier = $this->identifier;
         $helper->token = Tools::getAdminTokenLite('AdminModules');
-        $helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
+        $helper->currentIndex = AdminController::$currentIndex . '&configure=' . $this->name;
         $helper->default_form_language = $default_lang;
-        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
-
-        // Load current value
+        $helper->allow_employee_form_lang = $default_lang;
+        $helper->title = $this->displayName;
+        $helper->submit_action = 'submit' . $this->name;
         $helper->fields_value['GOOGLEMERCHANT_FEED_URL'] = Configuration::get('GOOGLEMERCHANT_FEED_URL');
 
         return $helper->generateForm($fields_form);
-    }
-
-    public function generateFeed()
-    {
-        // Get the products
-        $products = $this->getProducts();
-
-        // Start building the XML feed
-        $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0" xmlns:g="http://base.google.com/ns/1.0"></rss>');
-
-        $channel = $xml->addChild('channel');
-        $channel->addChild('title', 'Dealbrut Store');
-        $channel->addChild('link', 'https://dealbrut.com');
-        $channel->addChild('description', 'Dealbrut Product Feed');
-
-        foreach ($products as $product) {
-            $item = $channel->addChild('item');
-            $item->addChild('g:id', $product['id']);
-            $item->addChild('g:title', htmlspecialchars($product['title']));
-            $item->addChild('g:description', htmlspecialchars($product['description']));
-            $item->addChild('g:link', $product['link']);
-            $item->addChild('g:image_link', $product['image_link']);
-            $item->addChild('g:price', $product['price'] . ' USD');
-            $item->addChild('g:condition', $product['condition']);
-            $item->addChild('g:availability', $product['availability']);
-            $item->addChild('g:brand', $product['brand']);
-            $item->addChild('g:mpn', $product['mpn']);
-            $item->addChild('g:gtin', $product['gtin']);
-        }
-
-        // Save the feed
-        $feed_path = _PS_MODULE_DIR_ . $this->name . '/feed.xml';
-        $xml->asXML($feed_path);
-
-        return $xml->asXML();
     }
 
     private function getProducts()
@@ -135,7 +101,7 @@ class googlemerchant extends Module
                     m.name as brand,
                     p.reference as mpn,
                     p.ean13 as gtin,
-                    \'new\' as condition
+                    `new` as `condition`
                 FROM ' . _DB_PREFIX_ . 'product p
                 JOIN ' . _DB_PREFIX_ . 'product_lang pl ON (p.id_product = pl.id_product AND pl.id_lang = ' . (int)$this->context->language->id . ')
                 JOIN ' . _DB_PREFIX_ . 'manufacturer m ON (p.id_manufacturer = m.id_manufacturer)
@@ -145,9 +111,36 @@ class googlemerchant extends Module
         return Db::getInstance()->executeS($sql);
     }
 
+    public function generateFeed()
+    {
+        $products = $this->getProducts();
+        $xml = new SimpleXMLElement('<rss version="2.0" xmlns:g="http://base.google.com/ns/1.0"></rss>');
+        $channel = $xml->addChild('channel');
+        $channel->addChild('title', 'Dealbrut.com Product Feed');
+        $channel->addChild('link', 'https://dealbrut.com');
+        $channel->addChild('description', 'Google Merchant Center Feed for Dealbrut.com');
+
+        foreach ($products as $product) {
+            $item = $channel->addChild('item');
+            $item->addChild('g:id', $product['id']);
+            $item->addChild('g:title', $product['title']);
+            $item->addChild('g:description', $product['description']);
+            $item->addChild('g:link', $product['link']);
+            $item->addChild('g:image_link', $product['image_link']);
+            $item->addChild('g:price', $product['price']);
+            $item->addChild('g:availability', $product['availability']);
+            $item->addChild('g:brand', $product['brand']);
+            $item->addChild('g:mpn', $product['mpn']);
+            $item->addChild('g:gtin', $product['gtin']);
+            $item->addChild('g:condition', $product['condition']);
+        }
+
+        return $xml->asXML();
+    }
+
     public function logError($message)
     {
-        // Implement logging functionality if needed
-        error_log($message, 3, _PS_MODULE_DIR_ . $this->name . '/logs/errors.log');
+        $logFile = _PS_MODULE_DIR_ . $this->name . '/logs/feed_errors.log';
+        file_put_contents($logFile, date('Y-m-d H:i:s') . ' - ' . $message . PHP_EOL, FILE_APPEND);
     }
 }
