0a03e00bf6f34cc1b0d10b270962487e85d483d2
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-14 19:03:28 +0200

    Update googlemerchant.php

--- googlemerchant.php
+++ googlemerchant.php
@@ -1,5 +1,4 @@
 <?php
-
 if (!defined('_PS_VERSION_')) {
     exit;
 }
@@ -22,7 +21,9 @@ class googlemerchant extends Module
 
         $this->displayName = $this->l('Google Merchant Center Feed');
         $this->description = $this->l('Generate a product feed for Google Merchant Center.');
+
         $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+
         $this->feedFile = _PS_MODULE_DIR_ . 'googlemerchant/cache/feed.xml';
         $this->logFile = _PS_MODULE_DIR_ . 'googlemerchant/logs/feed_errors.log';
     }
@@ -38,6 +39,68 @@ class googlemerchant extends Module
         $this->context->controller->addJS($this->_path . 'views/js/googlemerchant.js');
     }
 
+    public function getContent()
+    {
+        $output = null;
+
+        if (Tools::isSubmit('submit' . $this->name)) {
+            $url = strval(Tools::getValue('GOOGLEMERCHANT_FEED_URL'));
+            if (!$url || empty($url)) {
+                $output .= $this->displayError($this->l('Invalid URL value'));
+            } else {
+                Configuration::updateValue('GOOGLEMERCHANT_FEED_URL', $url);
+                $output .= $this->displayConfirmation($this->l('Settings updated'));
+            }
+        }
+
+        return $output . $this->renderForm();
+    }
+
+    public function renderForm()
+    {
+        $fields_form = array(
+            'form' => array(
+                'legend' => array(
+                    'title' => $this->l('Google Merchant Center Feed'),
+                ),
+                'input' => array(
+                    array(
+                        'type' => 'text',
+                        'label' => $this->l('Feed URL'),
+                        'name' => 'GOOGLEMERCHANT_FEED_URL',
+                        'size' => 20,
+                        'required' => true,
+                    )
+                ),
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                    'class' => 'btn btn-default pull-right'
+                )
+            ),
+        );
+
+        $helper = new HelperForm();
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $helper->module = $this;
+        $helper->default_form_language = (int)Configuration::get('PS_LANG_DEFAULT');
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'submit' . $this->name;
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+            . '&configure=' . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+        $helper->tpl_vars = array(
+            'fields_value' => array(
+                'GOOGLEMERCHANT_FEED_URL' => Tools::getValue('GOOGLEMERCHANT_FEED_URL', Configuration::get('GOOGLEMERCHANT_FEED_URL')),
+            ),
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+        );
+
+        return $helper->generateForm(array($fields_form));
+    }
+
     public function generateFeed()
     {
         $products = $this->getProducts();
@@ -46,7 +109,7 @@ class googlemerchant extends Module
             return false;
         }
 
-        $xml = new SimpleXMLElement('<xml version="1.0" encoding="UTF-8"?><rss version="2.0" xmlns:g="http://base.google.com/ns/1.0"></rss>');
+        $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><rss version="2.0" xmlns:g="http://base.google.com/ns/1.0"></rss>');
         $channel = $xml->addChild('channel');
         $channel->addChild('title', Configuration::get('PS_SHOP_NAME'));
         $channel->addChild('link', Tools::getHttpHost(true) . __PS_BASE_URI__);
@@ -83,12 +146,13 @@ class googlemerchant extends Module
 
     public function getProducts()
     {
-        $sql = 'SELECT p.id_product, pl.name, pl.description_short, p.price, i.id_image, pl.link_rewrite, m.name as manufacturer_name, cl.name as category_name, p.ean13, p.weight, p.quantity
+        $sql = 'SELECT p.id_product, pl.name, pl.description_short, p.price, i.id_image, pl.link_rewrite, m.name as manufacturer_name, p.ean13, p.quantity, cl.name as category_name, gpc.google_product_category, p.weight
                 FROM ' . _DB_PREFIX_ . 'product p
-                LEFT JOIN ' . _DB_PREFIX_ . 'product_lang pl ON p.id_product = pl.id_product AND pl.id_lang = ' . (int)$this->context->language->id . '
+                JOIN ' . _DB_PREFIX_ . 'product_lang pl ON p.id_product = pl.id_product AND pl.id_lang = ' . (int)$this->context->language->id . '
                 LEFT JOIN ' . _DB_PREFIX_ . 'image i ON p.id_product = i.id_product AND i.cover = 1
                 LEFT JOIN ' . _DB_PREFIX_ . 'manufacturer m ON p.id_manufacturer = m.id_manufacturer
                 LEFT JOIN ' . _DB_PREFIX_ . 'category_lang cl ON p.id_category_default = cl.id_category AND cl.id_lang = ' . (int)$this->context->language->id . '
+                LEFT JOIN ' . _DB_PREFIX_ . 'google_product_category gpc ON p.id_product = gpc.id_product
                 WHERE p.active = 1';
 
         return Db::getInstance()->executeS($sql);
