1fdfa6236b0c1d18e6aed5a3b6659c982b49a3bc
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-15 00:24:49 +0200

    Update googlemerchant.php

--- googlemerchant.php
+++ googlemerchant.php
@@ -123,10 +123,13 @@ class googlemerchant extends Module
             $item->addChild('g:link', htmlspecialchars($this->context->link->getProductLink($product['id_product'], $product['link_rewrite'])));
             $item->addChild('g:description', htmlspecialchars(strip_tags($product['description_short'])));
 
-            // Ensuring the correct currency is used or fallback to ZAR if not set
-            $iso_code = isset($this->context->currency->iso_code) ? $this->context->currency->iso_code : 'ZAR';
-            $item->addChild('g:price', Tools::displayPrice($product['price'], $iso_code));
+            // Fix: Ensure valid currency ISO code, default to ZAR if not set
+            $currency = $this->context->currency;
+            if (!isset($currency) || !is_object($currency) || empty($currency->iso_code)) {
+                $currency = new Currency(Currency::getIdByIsoCode('ZAR'));
+            }
 
+            $item->addChild('g:price', Tools::displayPrice($product['price'], $currency->iso_code));
             $item->addChild('g:image_link', htmlspecialchars($this->context->link->getImageLink($product['link_rewrite'], $product['id_image'])));
             $item->addChild('g:availability', $product['quantity'] > 0 ? 'in stock' : 'out of stock');
             $item->addChild('g:brand', htmlspecialchars($product['manufacturer_name']) ?: 'Unknown');
