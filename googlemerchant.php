4f336acbe1501dbebfaf43cb9f9b3937c2384b5f
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-21 14:44:32 +0200

    Update googlemerchant.php

--- googlemerchant.php
+++ googlemerchant.php
@@ -121,15 +121,19 @@ class googlemerchant extends Module
             $item->addChild('g:id', htmlspecialchars($product['id_product']));
             $item->addChild('g:title', htmlspecialchars($product['name']));
             $item->addChild('g:link', htmlspecialchars($this->context->link->getProductLink($product['id_product'], $product['link_rewrite'])));
-            $item->addChild('g:description', htmlspecialchars(strip_tags($product['description_short'])));
-            $currency = isset($this->context->currency->iso_code) ? $this->context->currency->iso_code : 'ZAR';
-            $item->addChild('g:price', Tools::displayPrice($product['price'], new Currency(Currency::getIdByIsoCode($currency))));
+            $item->addChild('g:description', htmlspecialchars(strip_tags($product['description'])));
+            $item->addChild('g:price', Tools::displayPrice($product['price'], new Currency(Currency::getIdByIsoCode('ZAR'))));
             $item->addChild('g:image_link', htmlspecialchars($this->context->link->getImageLink($product['link_rewrite'], $product['id_image'])));
             $item->addChild('g:availability', $product['quantity'] > 0 ? 'in stock' : 'out of stock');
             $item->addChild('g:brand', htmlspecialchars($product['manufacturer_name']) ?: 'Unknown');
             $item->addChild('g:gtin', !empty($product['ean13']) ? htmlspecialchars($product['ean13']) : '');
             $item->addChild('g:mpn', htmlspecialchars($product['id_product']));
             $item->addChild('g:condition', 'new');
+
+            // Additional fields expected by Google
+            $item->addChild('g:product_type', htmlspecialchars($product['category_name']) ?? '');
+            $item->addChild('g:google_product_category', isset($product['google_product_category']) ? htmlspecialchars($product['google_product_category']) : '');
+            $item->addChild('g:shipping_weight', htmlspecialchars($product['weight']) . ' kg');
         }
 
         // Save the XML content to the feed.xml file
@@ -143,7 +147,7 @@ class googlemerchant extends Module
 
     public function getProducts()
     {
-        $sql = 'SELECT p.id_product, pl.name, pl.description_short, p.price, i.id_image, pl.link_rewrite, m.name as manufacturer_name, p.ean13, p.quantity, cl.name as category_name, p.weight
+        $sql = 'SELECT p.id_product, pl.name, pl.description, p.price, i.id_image, pl.link_rewrite, m.name as manufacturer_name, p.ean13, p.quantity, cl.name as category_name, p.weight
                 FROM ' . _DB_PREFIX_ . 'product p
                 JOIN ' . _DB_PREFIX_ . 'product_lang pl ON p.id_product = pl.id_product AND pl.id_lang = ' . (int)$this->context->language->id . '
                 LEFT JOIN ' . _DB_PREFIX_ . 'image i ON p.id_product = i.id_product AND i.cover = 1
