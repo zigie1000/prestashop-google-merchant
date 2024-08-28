efafe507d7048224fc2919cbf4f329c514818a23
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-08-29 00:44:26 +0200

    Update googlemerchant.php

--- googlemerchant.php
+++ googlemerchant.php
@@ -8,6 +8,7 @@ class googlemerchant extends Module
 {
     private $feedFile;
     private $logFile;
+    private $mappingFile;
 
     public function __construct()
     {
@@ -27,6 +28,7 @@ class googlemerchant extends Module
 
         $this->feedFile = _PS_MODULE_DIR_ . 'googlemerchant/feed.xml';
         $this->logFile = _PS_MODULE_DIR_ . 'googlemerchant/logs/feed_errors.log';
+        $this->mappingFile = _PS_MODULE_DIR_ . 'googlemerchant/mapping.csv';
     }
 
     public function install()
@@ -110,6 +112,8 @@ class googlemerchant extends Module
             return false;
         }
 
+        $categoryMapping = $this->loadCategoryMapping();
+
         $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><rss version="2.0" xmlns:g="http://base.google.com/ns/1.0"></rss>');
         $channel = $xml->addChild('channel');
         $channel->addChild('title', Configuration::get('PS_SHOP_NAME'));
@@ -122,21 +126,19 @@ class googlemerchant extends Module
             $item->addChild('g:title', htmlspecialchars($product['name']));
             $item->addChild('g:link', htmlspecialchars($this->context->link->getProductLink($product['id_product'], $product['link_rewrite'])));
             $item->addChild('g:description', htmlspecialchars(strip_tags($product['description'])));
-            
-            // Fix for price format
-            $price = number_format((float)$product['price'], 2, '.', '') . ' ZAR';
-            $item->addChild('g:price', $price);
-
+            $item->addChild('g:price', number_format($product['price'], 2, '.', '') . ' ZAR');
             $item->addChild('g:image_link', htmlspecialchars($this->context->link->getImageLink($product['link_rewrite'], $product['id_image'])));
             $item->addChild('g:availability', $product['quantity'] > 0 ? 'in stock' : 'out of stock');
             $item->addChild('g:brand', htmlspecialchars($product['manufacturer_name']) ?: 'Unknown');
-            $item->addChild('g:gtin', !empty($product['ean13']) ? htmlspecialchars($product['ean13']) : '');
+            $item->addChild('g:gtin', !empty($product['ean13']) ? htmlspecialchars($product['ean13']) : 'null');
             $item->addChild('g:mpn', htmlspecialchars($product['id_product']));
             $item->addChild('g:condition', 'new');
 
             // Additional fields expected by Google
-            $item->addChild('g:product_type', htmlspecialchars($product['category_name']) ?? '');
-            $item->addChild('g:google_product_category', isset($product['google_product_category']) ? htmlspecialchars($product['google_product_category']) : '');
+            $productCategory = htmlspecialchars($product['category_name']) ?? '';
+            $googleCategory = isset($categoryMapping[$productCategory]) ? $categoryMapping[$productCategory] : '';
+            $item->addChild('g:product_type', $productCategory);
+            $item->addChild('g:google_product_category', $googleCategory);
             $item->addChild('g:shipping_weight', htmlspecialchars($product['weight']) . ' kg');
         }
 
@@ -149,7 +151,7 @@ class googlemerchant extends Module
         exit;
     }
 
-    public function getProducts()
+    private function getProducts()
     {
         $sql = 'SELECT p.id_product, pl.name, pl.description, p.price, i.id_image, pl.link_rewrite, m.name as manufacturer_name, p.ean13, p.quantity, cl.name as category_name, p.weight
                 FROM ' . _DB_PREFIX_ . 'product p
@@ -165,5 +167,21 @@ class googlemerchant extends Module
     private function logError($message)
     {
         file_put_contents($this->logFile, date('Y-m-d H:i:s') . ' - ' . $message . PHP_EOL, FILE_APPEND);
+    }
+
+    private function loadCategoryMapping()
+    {
+        $mapping = [];
+
+        if (($handle = fopen($this->mappingFile, "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                if (count($data) == 2) {
+                    $mapping[$data[0]] = $data[1];
+                }
+            }
+            fclose($handle);
+        }
+
+        return $mapping;
     }
 }
